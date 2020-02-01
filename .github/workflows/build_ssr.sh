d86d6ad8db70063708df672c2ae037c5d227cdfc
donmahallem <donmahallem@users.noreply.github.com>
2020-02-01 16:54:54 +0100

    add pm2 ls step

--- .github/workflows/build_ssr.sh
+++ .github/workflows/build_ssr.sh
@@ -3,6 +3,8 @@ npm install -g pm2
 npm run build:ssr
 pm2 start ./dist/server/main.js --no-autorestart
 sleep 5
+pm2 ls
+sleep 5
 curl localhost:4000 > ./dist/browser/405.html
 pm2 stop all
 pm2 delete all
