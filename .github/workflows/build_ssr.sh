345af6e9b5716813253372fc09e0481dab769919
donmahallem <donmahallem@users.noreply.github.com>
2020-02-01 16:31:14 +0100

    add build_ssr.sh

---
+++ .github/workflows/build_ssr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+npm install -g pm2
+npm run build:ssr
+pm2 start ./dist/server/main.js --no-autorestart
+sleep 5
+curl localhost:4000 > ./dist/browser/405.html
+pm2 stop all
+pm2 delete all
