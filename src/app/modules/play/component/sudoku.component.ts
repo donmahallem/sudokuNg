be45e6d1fbd6ca36a9e58bdc9f140845ec089506
donmahallem <donmahallem@users.noreply.github.com>
2020-02-01 09:48:56 +0100

    Bump

--- src/app/modules/play/component/sudoku.component.ts
+++ src/app/modules/play/component/sudoku.component.ts
@@ -7,7 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SudokuComponent {
     title = 'sudokuNg';
+    private field: number[][] = new Array(9).fill(0).map(val => new Array(9).fill(0));
     public setValue(x: number, y: number, value: number): void {
         console.log(x, y, value);
+        this.field[x][y] = value;
+        console.log(this.field);
     }
 }
