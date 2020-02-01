60e207987b9ba8964c4158b299deb63dac50cd7a
donmahallem <donmahallem@users.noreply.github.com>
2020-02-01 10:51:26 +0100

    Bump

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing'; // tslint:disable:component-selector
+import { Component } from '@angular/core';
+// tslint:disable:directive-selector
+@Component({
+  selector: 'mat-toolbar',
+  template: '<div></div>',
+})
+class TestMatToolbarComponent {
+}
 
+
+// tslint:enable:component-selector
+// tslint:enable:directive-selector
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        TestMatToolbarComponent
+      ],
+      imports: [
+        RouterTestingModule
       ],
     }).compileComponents();
   }));
